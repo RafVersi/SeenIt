5d0a05b6959fb5af1b9835657ec8e3b711a79aba
RafVersi <raf.versichele@gmail.com>
2025-08-14 16:02:29 +0200

    introducing the link between author and movie

--- back-end/model/movie.ts
+++ back-end/model/movie.ts
@@ -1,16 +1,19 @@
-import { Movie as MoviePrisma } from '@prisma/client';
+import { Movie as MoviePrisma, Author as AuthorPrisma } from '@prisma/client';
+import { Author } from './author';
 
 export class Movie{
     private id?: number;
     private name: string;
     private year: number;
     private img: string;
+    private authorId: Author
 
-    constructor(movie: { id?: number; name: string; year: number; img: string}){
+    constructor(movie: { id?: number; name: string; year: number; img: string; authorId: Author}){
         this.id = movie.id;
         this.name = movie.name;
         this.year = movie.year;
         this.img = movie.img;
+        this.authorId = movie.authorId;
     }
 
     setId(id: number){
@@ -25,6 +28,9 @@ export class Movie{
     setFullname(img: string){
         this.img = img;
     }
+    setAuthorId(author: Author){
+        this.authorId = author;
+    }
 
     getId(): number | undefined {
         return this.id;
@@ -38,22 +44,29 @@ export class Movie{
     getImg(): string {
         return this.img;
     }
+    getAuthorId(): Author {
+        return this.authorId;
+    }
 
     equals(movie: Movie): boolean {
         return (
             this.id === movie.getId() &&
             this.name === movie.getName() &&
             this.year === movie.getYear() &&
-            this.img === movie.getImg() 
+            this.img === movie.getImg() &&
+            this.authorId === movie.getAuthorId()
         )
     }
 
-    static from({id, name, year, img}: MoviePrisma){
+    static from({id, name, year, img, authorId}: MoviePrisma & {
+        authorId: AuthorPrisma;
+    }){
             return new Movie({
                 id,
                 name,
                 year,
-                img
+                img,
+                authorId: Author.from(authorId)
             });
         }
 }
